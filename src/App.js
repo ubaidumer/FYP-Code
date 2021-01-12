9b45337011be661c174a94c0c8623ff824f17e0d
azeemsultan <43080738+azeemsultan@users.noreply.github.com>
2021-01-12 06:51:06 +0500

    HAHA SUCCESS

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Route, BrowserRouter} from "react-router-dom";
 
 import serviceproviderSignup from "./Component/common/serviceproviderSignup";
 import customerSignup from "./Component/common/customerSignup";
-import Login from "./Component/common/login";
-import postatask from "./Component/customer/postATask";
-import acceptRequest from "./Component/serviceprovider/AcceptRequest";
-import workInProgress from "./Component/serviceprovider/workInProgress";
+
+
 import acceptServiceProvider from "./Component/customer/acceptServiceProvider";
-import orderHistory from "./Component/customer/OrderHistory";
-import workHistory from "./Component/serviceprovider/workHistory";
+
 import payment from "./Component/customer/payment";
 import rating from "./Component/customer/rating";
 
 import adminLogin from "./Component/common/adminlogin";
-import HomeScreen from "./Component/common/HomeScreen";
-import Footer from "./Component/common/Footer";
 
 import NBSMap from "./Component/common/NearbyServices"
 import FLMap from "./Component/common/FindLocationMap";
@@ -25,7 +20,7 @@ import SignIn from "./Component/common/SignIn";
 import LoginPage from "./Component/admin/LoginPage";
 import ProfilePage from "./Component/customer/ProfilePage";
 import SProfilePage from "./Component/serviceprovider/ServiceProfile";
-import PostedTask from "./Component/customer/postedTask";
+
 import Stripecard from"./Component/customer/Stripecard";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
@@ -51,22 +46,18 @@ class App extends Component {
                <Route path="/sprofile" component={SProfilePage}/>   
               <Route path="/adminLogin" component={adminLogin}/>   
               <Route path="/stripecard" component={Stripecard}/>
-             <Route path="/home" component={Home}/>
+             
               <Route path="/customerdashboard" component={customerDashboard}/>      
               <Route path="/serviceproviderSignup" exact={true} component={serviceproviderSignup} />
               <Route path="/customerSignup" component={customerSignup} />
               <Route path="/login" component={SignIn}/>
-              <Route path="/postatask" component={postatask}/>
-              <Route path="/acceptRequest" component={acceptRequest}/>
-              <Route path="/workInProgress" component={workInProgress}/>
+              
               <Route path="/acceptServiceProvider" component={acceptServiceProvider}/>
               <Route path="/payment" component={payment}/>
-              <Route path="/orderHistory" component={orderHistory}/>
-              <Route path="/postedTask" component={PostedTask}/>
-              <Route path="/workHistory" component={workHistory}/>
+              
               <Route path="/rating" component={rating}/>
       
-              <Route path="/" exact={true} component={HomeScreen}/>
+              <Route path="/" component={Home}/>
               </Elements>
               <Route path="/dashboard" component={Dashboard}/>
               <Route path="/FindLocationMap" component={FLMap}/>
